14b6832151a9c5fe1aa8c84a37347b96e8607d36
TibCVA <thibaut.lacour@cva-france.fr>
2024-12-29 16:22:24 +0100

    Update indicators.py

--- modules/indicators.py
+++ modules/indicators.py
@@ -1,1 +1,36 @@
+import pandas as pd
+import ta
+
+def compute_indicators(df):
+    """
+    df => [date, open, high, low, close, volume, market_cap]
+    => calcule ATR(14), RSI(14), MACD(12,26,9)
+    """
+    dff = df.copy()
+    for col in ["open","high","low","close","volume"]:
+        dff[col] = pd.to_numeric(dff[col], errors="coerce").fillna(0)
+
+    # ATR
+    atr_ind = ta.volatility.AverageTrueRange(
+        high=dff["high"],
+        low=dff["low"],
+        close=dff["close"],
+        window=14
+    )
+    dff["ATR"] = atr_ind.average_true_range()
+
+    # RSI
+    rsi_ind = ta.momentum.RSIIndicator(dff["close"], window=14)
+    dff["RSI"] = rsi_ind.rsi()
+
+    # MACD
+    macd_ind = ta.trend.MACD(
+        close=dff["close"],
+        window_slow=26,
+        window_fast=12,
+        window_sign=9
+    )
+    dff["MACD"] = macd_ind.macd_diff()
+
+    return dff
 
