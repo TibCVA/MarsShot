6ea7bc906b2c4c08e7c1c2deb745857c7998ac4c
TibCVA <thibaut.lacour@cva-france.fr>
2024-12-29 16:23:38 +0100

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -1,1 +1,16 @@
+import requests
+import logging
+
+def send_telegram_message(config, msg):
+    tg = config.get("telegrams",{})
+    bot_token = tg.get("bot_token","")
+    chat_id = tg.get("chat_id","")
+    if not(bot_token and chat_id):
+        return
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    data = {"chat_id": chat_id,"text": msg}
+    try:
+        requests.post(url, data=data)
+    except Exception as e:
+        logging.error(f"[Telegram Error] {e}")
 
