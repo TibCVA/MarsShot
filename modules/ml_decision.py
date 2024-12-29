d84e5c51c2a4ccf1bad2fc803e3b39ec9d7507e3
TibCVA <thibaut.lacour@cva-france.fr>
2024-12-29 16:22:39 +0100

    Update ml_decision.py

--- modules/ml_decision.py
+++ modules/ml_decision.py
@@ -1,1 +1,20 @@
+import joblib
+import numpy as np
+
+model = joblib.load("/app/model.pkl")
+
+def get_buy_signal(features, threshold=0.7):
+    arr = np.array([
+        features["price"],
+        features["volume"],
+        features["market_cap"],
+        features["holders"],
+        features["sentiment_score"],
+        features["ATR"],
+        features["RSI"],
+        features["MACD"]
+    ]).reshape(1,-1)
+
+    prob = model.predict_proba(arr)[0][1]
+    return (prob >= threshold), prob
 
