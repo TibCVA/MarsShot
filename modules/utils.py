5beb8287d7a122236da1798b5449fb6f17de4301
TibCVA <thibaut.lacour@cva-france.fr>
2025-01-03 18:39:43 +0100

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -2,6 +2,9 @@ import logging
 import requests
 
 def send_telegram_message(bot_token, chat_id, text):
+    """
+    Envoie un message 'text' au chat_id via le bot Telegram dont le token est 'bot_token'.
+    """
     if not bot_token or not chat_id:
         return
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
