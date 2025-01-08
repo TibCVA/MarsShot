b653704370d94509ffc579161065efcfe290e447
TibCVA <thibaut.lacour@cva-france.fr>
2025-01-08 16:02:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ except ImportError:
 def main():
     """
     Boucle principale du bot de trading en mode LIVE.
-    - Chaque jour à 15h00 heure de Paris => daily_update_live(...) => SELL/BUY
+    - Chaque jour à 16h10 heure de Paris => daily_update_live(...) => SELL/BUY
     - Intraday => intraday_check_real(...) => trailing/stop-loss live
     - Stockage local (positions_meta) dans bot_state.json
     """
@@ -69,22 +69,22 @@ def main():
             hour_p = now_paris.hour
             min_p = now_paris.minute
 
-            # => Tâche daily à 15h00 PARIS
+            # => Tâche daily à 16h10 PARIS
             if (
-                hour_p == 15
-                and min_p == 0
+                hour_p == 16
+                and min_p == 10
                 and not state.get("did_daily_update_today", False)
             ):
-                logging.info("[MAIN] It's 15h00 in Paris => launching daily_update_live.")
+                logging.info("[MAIN] It's 16h10 in Paris => launching daily_update_live.")
                 daily_update_live(state, config, bexec)
                 state["did_daily_update_today"] = True
                 save_state(state)
                 logging.info("[MAIN] daily_update_today flag => True.")
 
-            # Reset du flag si on n'est plus à 15h
-            if hour_p != 15:
+            # Reset du flag si on n'est plus à 16h
+            if hour_p != 16:
                 if state.get("did_daily_update_today", False):
-                    logging.info("[MAIN] hour!=15 => reset did_daily_update_today=False.")
+                    logging.info("[MAIN] hour!=16 => reset did_daily_update_today=False.")
                 state["did_daily_update_today"] = False
                 save_state(state)
 
