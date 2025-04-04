11a5db724a3992ffa7c83e23fe372815cb59110b
TibCVA <thibaut.lacour@cva-france.fr>
2025-04-04 15:52:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -229,8 +229,8 @@ def main():
     tz_paris = pytz.timezone("Europe/Paris")
 
     # Daily update live fixée à 01h20 UTC
-    DAILY_UPDATE_HOUR = 1
-    DAILY_UPDATE_MIN = 15
+    DAILY_UPDATE_HOUR = 2
+    DAILY_UPDATE_MIN = 10
 
     while True:
         try:
