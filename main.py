044acb399b89b3c0dd63565ef5549c356f260df5
TibCVA <thibaut.lacour@cva-france.fr>
2025-07-17 11:13:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -494,7 +494,7 @@ def main():
         return
         
     DAILY_UPDATE_HOUR_UTC = config.get("strategy", {}).get("daily_update_hour_utc", 09) 
-    DAILY_UPDATE_MINUTE_UTC = config.get("strategy", {}).get("daily_update_minute_utc", 15) 
+    DAILY_UPDATE_MINUTE_UTC = config.get("strategy", {}).get("daily_update_minute_utc", 30) 
 
     logger.info(f"Boucle principale démarrée. Mise à jour quotidienne prévue à {DAILY_UPDATE_HOUR_UTC:02d}:{DAILY_UPDATE_MINUTE_UTC:02d} UTC.")
 
