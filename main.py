f47a30096a79cab65051f5e2d10f2578667c1217
TibCVA <thibaut.lacour@cva-france.fr>
2025-11-05 08:42:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -502,8 +502,8 @@ def main():
         logger.critical(f"Erreur initialisation TradeExecutor: {e}. Arrêt.", exc_info=True)
         return
         
-    DAILY_UPDATE_HOUR_UTC = config.get("strategy", {}).get("daily_update_hour_utc", 02) 
-    DAILY_UPDATE_MINUTE_UTC = config.get("strategy", {}).get("daily_update_minute_utc", 00) 
+    DAILY_UPDATE_HOUR_UTC = config.get("strategy", {}).get("daily_update_hour_utc", 2) 
+    DAILY_UPDATE_MINUTE_UTC = config.get("strategy", {}).get("daily_update_minute_utc", 0) 
 
     logger.info(f"Boucle principale démarrée. Mise à jour quotidienne prévue à {DAILY_UPDATE_HOUR_UTC:02d}:{DAILY_UPDATE_MINUTE_UTC:02d} UTC.")
 
